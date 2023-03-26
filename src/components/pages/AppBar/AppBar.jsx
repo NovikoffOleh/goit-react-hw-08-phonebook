7e7dc731c80e0380f3138f3d3f34cde435c48ca5
NovikoffOleh <novikoff10000@gmail.com>
2023-03-26 22:40:35 +0300

    Update AppBar.jsx

--- src/components/pages/AppBar/AppBar.jsx
+++ src/components/pages/AppBar/AppBar.jsx
@@ -10,7 +10,7 @@ export const AppBar = () => {
     <header>
       <ul className={s.list}>
         <li className={s.items}>
-          <Link to="/">Додому</Link>
+          <Link to="/">До дому</Link>
         </li>
 
         {!isAuth && (
